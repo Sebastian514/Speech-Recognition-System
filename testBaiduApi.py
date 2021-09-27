cef2d79cbe93b0a1ecbf89d027f2bc27fdfba6fe
sebastian514 <541150041@qq.com>
2021-09-28 01:06:25 +0800

    firstPullRequest

---
+++ testBaiduApi.py
@@ -0,0 +1,6 @@
+from aip import AipSpeech
+APP_ID ='24917387'
+API_KEY = '4DzTBjliiLURreWKIbfCkdRf'
+SECRET_KEY = 'uv0XPf1iUYks6DT0yBw1lwuYhsVTmNRG'
+
+client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
